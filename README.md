3498c7c8429a18ac9be52c86696e7f66bef7446e
Luiz Rattacaso <167656308+Luizrattacaso@users.noreply.github.com>
2025-07-09 11:45:40 +0000

    update script

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # DescobertaDeRede
-ferramenta de descoberta de rede com shell script
+Script para melhor entendimento do shell script
+
+Criação de ferramenta de descoberta de rede com shell script.
---
+++ script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Enter the first three octets of your IP (e.g., 192.168.1): "
+read mask
+
+for i in {0..254}; do
+    (
+    if ping -c 1 -W 1 "$mask.$i" | grep -q ttl; then
+        echo "$mask.$i is alive"
+    fi 
+    ) &
+done
+
+wait
