2b16994afe5052b90666aa9f6ba943a1490356f5
Luiz Rattacaso <167656308+Luizrattacaso@users.noreply.github.com>
2025-07-09 12:03:49 +0000

    teste

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Enter the first three octets of your IP (e.g., 192.168.1): "
 read mask
-
+echo ""
 for i in {0..254}; do
     (
     if ping -c 1 -W 1 "$mask.$i" | grep -q ttl; then
